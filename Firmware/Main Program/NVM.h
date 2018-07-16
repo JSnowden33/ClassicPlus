836b2fbadf5b202acec346ce1e4de0e6e0f5be0e
JSnowden33 <34112118+JSnowden33@users.noreply.github.com>
2018-07-15 23:52:08 -0400

    Add EEPROM addresses

--- Firmware/Main Program/NVM.h
+++ Firmware/Main Program/NVM.h
@@ -6,6 +6,22 @@
 #ifndef _NVM_H_
 #define	_NVM_H_
 
+// Expansion/camera calibration registers
+#define EE_REG_LX_MIN   0x00
+#define EE_REG_LY_MIN   0x01
+#define EE_REG_LX_MAX   0x02
+#define EE_REG_LY_MAX   0x03
+#define EE_REG_RX_MIN   0x04
+#define EE_REG_RY_MIN   0x05
+#define EE_REG_RX_MAX   0x06
+#define EE_REG_RY_MAX   0x07
+#define EE_REG_DZ_L     0x08
+#define EE_REG_DZ_R     0x09
+#define EE_REG_INVERT1  0x0A
+#define EE_REG_INVERT2  0x0B
+#define EE_REG_CONFIG   0x0C
+#define EE_REG_IR_SENS  0x0D
+
 void NVMunlock();
 
 void EEwrite(u8 addr, u8 *buf, u8 length);
